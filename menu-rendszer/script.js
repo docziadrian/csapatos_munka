bcd19e1a71f36e3ecef0676d517d829b165a8d7b
docziadrian <docziadrianmark@turr.hu>
2023-01-14 12:34:11 +0100

    Menü script linkelése

--- menu-rendszer/script.js
+++ menu-rendszer/script.js
@@ -1,6 +1,6 @@
 //Egységes menü rendszer kialakítása:
 const nav = document.querySelector(".navbar"); //osztály alapján kiválasztja a menüt
-fetch("../../csapatatos_munka/tree/CM-64/menu-rendszer/navbar.html") //Egy template -re hivatkozik, amiben a menü van
+fetch("../../csapatatos_munka/CM-64/menu-rendszer/navbar.html") //Egy template -re hivatkozik, amiben a menü van
   .then((res) => res.text()) //=> function
   .then((data) => {
     nav.innerHTML = data;
@@ -16,6 +16,5 @@ function myFunction() {
 }
 
 function ugras_a_kosar_oldalra() {
-  window.location =
-    "../../csapatatos_munka/tree/CM-64/bevasarlo-kosar/kosar.html";
+  window.location = "../../csapatatos_munka/CM-64/bevasarlo-kosar/kosar.html";
 }
