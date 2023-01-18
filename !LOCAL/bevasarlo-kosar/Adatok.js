bc01914a3add88f0779e5729422da91aa409d520
docziadrian <docziadrianmark@turr.hu>
2023-01-18 18:59:31 +0100

    Localis változat

---
+++ !LOCAL/bevasarlo-kosar/Adatok.js
@@ -0,0 +1,73 @@
+let targyakAdata = [
+  {
+    id: "bibor_festek",
+    name: "Bíbor festék",
+    price: "5000",
+    img: "/Pictures/festekek/vodorbibor.png",
+    img_siker: "/Pictures/succes_hibatlan.gif",
+    hidden: "hidden1",
+  },
+
+  {
+    id: "rozsaszin_festek",
+    name: "Rózsaszín festék",
+    price: "5000",
+    img: "/Pictures/festekek/rozsaszinvodor.png",
+    img_siker: "/Pictures/succes_hibatlan.gif",
+    hidden: "hidden2",
+  },
+
+  {
+    id: "lila_festek",
+    name: "Kék festék",
+    price: "5500",
+    img: "/Pictures/festekek/vodorkek.png",
+    img_siker: "/Pictures/succes_hibatlan.gif",
+    hidden: "hidden3",
+  },
+
+  {
+    id: "narancs_sarga_festek",
+    name: "Narancs festék",
+    price: "4500",
+    img: "/Pictures/festekek/vodornarancs.png",
+    img_siker: "/Pictures/succes_hibatlan.gif",
+    hidden: "hidden4",
+  },
+
+  {
+    id: "piros_festek",
+    name: "Piros festék",
+    price: "5000",
+    img: "/Pictures/festekek/vodorpiros.png",
+    img_siker: "/Pictures/succes_hibatlan.gif",
+    hidden: "hidden5",
+  },
+
+  {
+    id: "sarga_festek",
+    name: "Sárga festék",
+    price: "4500",
+    img: "/Pictures/festekek/vodorsarga.png",
+    img_siker: "/Pictures/succes_hibatlan.gif",
+    hidden: "hidden6",
+  },
+
+  {
+    id: "kek_festek_fixed",
+    name: "Kék festék",
+    price: "4500",
+    img: "/Pictures/festekek/vodorteal.png",
+    img_siker: "/Pictures/succes_hibatlan.gif",
+    hidden: "hidden7",
+  },
+
+  {
+    id: "zold_festek_fixed",
+    name: "Zöld festék",
+    price: "4500",
+    img: "/Pictures/festekek/vodorzold.png",
+    img_siker: "/Pictures/succes_hibatlan.gif",
+    hidden: "hidden8",
+  },
+];
